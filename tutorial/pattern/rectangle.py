6b0110e54328fedd4518324fcc65102912f942fc
luft <araryap@gmail.com>
2024-07-30 15:36:15 +0700

    fix import

--- tutorial/pattern/rectangle.py
+++ tutorial/pattern/rectangle.py
@@ -1,13 +1,10 @@
-from ili934xnew import ILI9341
+from ili9341 import ILI9341
 from machine import Pin, SPI
 from time import sleep
 
 spi = SPI(0, baudrate=20000000, miso=Pin(20), mosi=Pin(19), sck=Pin(18))
 display = ILI9341(spi, cs=Pin(17), dc=Pin(27), rst=Pin(16), w=320, h=240, r=2)
 display.erase()
-
-width = 320
-height = 240
 
 x_start = 50
 y_start = 50
